ed9f6fa5ad28e7098a4e2463389e6bb816c0eef0
jack1799 <jack1799@mail.ru>
2016-04-12 14:26:28 +0300

    1

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -1,55 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication4
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double[] a = { 5, 4, 3, 2, 1 };
-            Console.WriteLine("Был массив");
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.Write(a[i]+" ");
-            }
-            Console.WriteLine();
-            QuickSort(a, 0, a.Length - 1);
-            Console.WriteLine("Получили массив");
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.Write(a[i] + " ");
-            }
-            Console.WriteLine();
-        }
-        static void QuickSort(double[] array, int FirstArrayIndex, int SecondArrayIndex)
-        {
-            int FirstChangingIndex = FirstArrayIndex;
-            int SecondChangingIndex = SecondArrayIndex;
-            double ReferenceElement;
-            if (SecondArrayIndex > FirstArrayIndex)
-            {
-                ReferenceElement = array[(FirstArrayIndex + SecondArrayIndex) / 2];
-                while (FirstChangingIndex <= SecondChangingIndex)
-                {
-                    while ((FirstChangingIndex < SecondArrayIndex) && (array[FirstChangingIndex] < ReferenceElement)) ++FirstChangingIndex;             
-                    while ((SecondChangingIndex > FirstArrayIndex) && (array[SecondChangingIndex] > ReferenceElement)) --SecondChangingIndex;
-                   if (FirstChangingIndex <= SecondChangingIndex)
+﻿using System; 
+ using System.Collections.Generic; 
+ using System.Linq; 
+ using System.Text; 
+ using System.Threading.Tasks; 
+ 
+ 
+ namespace ConsoleApplication4
+ { 
+     class Program
+     { 
+         static void Main(string[] args)
+         { 
+             double[] a = { 5, 4, 3, 2, 1 }; 
+             Console.WriteLine("Был массив"); 
+             for (int i = 0; i < a.Length; i++) 
+             { 
+                 Console.Write(a[i]+" "); 
+             } 
+             Console.WriteLine(); 
+             QuickSort(a, 0, a.Length - 1); 
+             Console.WriteLine("Получили массив"); 
+             for (int i = 0; i < a.Length; i++) 
+             { 
+                 Console.Write(a[i] + " "); 
+             } 
+             Console.WriteLine(); 
+         } 
+         static void QuickSort(double[] array, int firstArrayIndex, int secondArrayIndex)
+         { 
+             int firstChangingIndex = firstArrayIndex; 
+             int secondChangingIndex = secondArrayIndex; 
+             double referenceElement; 
+             if (secondArrayIndex > firstArrayIndex) 
+             { 
+                 referenceElement = array[(firstArrayIndex + secondArrayIndex) / 2]; 
+                 while (firstChangingIndex <= secondChangingIndex) 
+                 {
+                    while ((firstChangingIndex < secondArrayIndex) && (array[firstChangingIndex] < referenceElement))
                     {
-                        double T;
-                        T = array[FirstChangingIndex];
-                        array[FirstChangingIndex] = array[SecondChangingIndex];
-                        array[SecondChangingIndex] = T;
-                        ++FirstChangingIndex;
-                        --SecondChangingIndex;
+                        ++firstChangingIndex;
                     }
-                } 
-                if (FirstArrayIndex < SecondChangingIndex) QuickSort(array, FirstArrayIndex, SecondChangingIndex);               
-                if (FirstChangingIndex < SecondArrayIndex) QuickSort(array, FirstChangingIndex, SecondArrayIndex);
-            }
-        }
-    }
-}
+                    while ((secondChangingIndex > firstArrayIndex) && (array[secondChangingIndex] > referenceElement))
+                    {
+                        --secondChangingIndex;
+                    }
+                    if (firstChangingIndex <= secondChangingIndex) 
+                     { 
+                         double t; 
+                         t = array[firstChangingIndex]; 
+                         array[firstChangingIndex] = array[secondChangingIndex]; 
+                         array[secondChangingIndex] = t; 
+                         ++firstChangingIndex; 
+                         --secondChangingIndex; 
+                     } 
+                 }
+                if (firstArrayIndex < secondChangingIndex)
+                {
+                    QuickSort(array, firstArrayIndex, secondChangingIndex);
+                }
+                if (firstChangingIndex < secondArrayIndex)
+                {
+                    QuickSort(array, firstChangingIndex, secondArrayIndex);
+                }
+              } 
+         } 
+     } 
+ } 
